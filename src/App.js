588a0d789c555529e02b89d4d91030b3cfbaf9a1
mpoapostolis <a.mpostanis@logicea.com>
2017-04-28 17:52:47 +0300

    keyPad

--- src/App.js
+++ src/App.js
@@ -1,102 +1,64 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
+import React, { PropTypes } from 'react'
+const { func }=PropTypes
 
-class App extends Component {
-  state = {
-    apotelesma: 0,
-    num: '',
-    praksiAigain: true,
-    currentNum: '',
-    sum: 0
-  }
+class Calc extends React.Component {
+	state={
+		value: '',
+		dot: false,
+	}
+	static propTypes ={
+		toggleStakeKeypad: func,
+		setSinglesStake: func,
+	}
 
-  calculations(key){
-    const { sum, num, currentNum } = this.state
-    switch (key) {
-      case '+':
-        this.setState({sum: sum + parseInt(currentNum)})
-        break;
-      case '-':
-        this.setState({sum: sum + parseInt(currentNum)})
-        break;
-      case '*':
-        this.setState({sum: sum * parseInt(currentNum)})
-        break;
-      case '/':
-        this.setState({sum: sum / parseInt(currentNum)})
-        break;
-      default:
+	pressButton(key) {
+		const { value, dot }=this.state
+		switch (key) {
+			case 'C':
+				this.setState({ value: '' })
+				break;
+			case 'OK':
+				break;
+			case '.':
+				!dot && value.length && this.setState({ dot: true, value: value + key })
+				break;
+			default:
+				this.setState({ value: value + key })
+				break;
+		}
+	}
 
-    }
-  }
-
-  isClicked(key){
-    const praksis = ['+', '-', '*', '/']
-    const { sum, num, praksiAigain, currentNum } = this.state
-    const isPraksi = praksis.find((praksi)=> key === praksi)
-    const isNumber = typeof key === 'number'
-    if (isNumber){
-      this.setState({num: num + key, currentNum: currentNum + key, praksiAigain: true})
-    }
-    if ( isPraksi && praksiAigain ) {
-      this.calculations(key)
-      this.setState({num: num + key, currentNum : '', praksiAigain: false})
-    }
-  }
-
-  render() {
-    const { num, sum } = this.state
-    console.log(sum);
-    return (
-      <div>
-        <div className ="display">
-          <div className="up">{num}</div>
-          <div className="down">1+5+5</div>
-        </div>
-
-        <table>
-          <tr>
-            <td></td>
-            <td></td>
-            <td> ON </td>
-            <td> OFF </td>
-          </tr>
-
-          <tr>
-            <td onClick = {()=>this.isClicked(7)}> 7 </td>
-            <td onClick = {()=>this.isClicked(8)}> 8 </td>
-            <td onClick = {()=>this.isClicked(9)}> 9 </td>
-            <td onClick = {()=>this.isClicked('+')}> + </td>
-          </tr>
-
-          <tr>
-            <td onClick = {()=>this.isClicked(4)}> 4 </td>
-            <td onClick = {()=>this.isClicked(5)}> 5 </td>
-            <td onClick = {()=>this.isClicked(6)}> 6 </td>
-            <td onClick = {()=>this.isClicked('-')}> - </td>
-          </tr>
-
-          <tr>
-            <td onClick = {()=>this.isClicked(1)}> 1 </td>
-            <td onClick = {()=>this.isClicked(2)}> 2 </td>
-            <td onClick = {()=>this.isClicked(3)}> 3 </td>
-            <td onClick = {()=>this.isClicked('*')}> x </td>
-          </tr>
-
-          <tr>
-            <td onClick = {()=>this.isClicked(0)}> 0 </td>
-            <td onClick = {()=>this.isClicked('.')}> . </td>
-            <td onClick = {()=>this.isClicked('=')}> = </td>
-            <td onClick = {()=>this.isClicked('/')}> / </td>
-          </tr>
-
-        </table>
-
-        <p>{this.state.apotelesma}</p>
-      </div>
-    );
-  }
+	render() {
+		const { value }=this.state
+		const values=[
+			[{ value: '7' }, { value: '8' }, { value: '9' }, { value: 'C', className: 'C', rowSpan: '2' }],
+			[{ value: '4' }, { value: '5' }, { value: '6' }],
+			[{ value: '1' }, { value: '2' }, { value: '3' }, { value: 'OK', className: 'OK', rowSpan: '2' }],
+			[{ value: '0', colSpan: '2' }, { value: '.' }]]
+		return (
+			<div className='calcContainer'>
+				<div className='modalBack'>
+					<div className='calcBody'>
+						<div className='display'>
+							<input autoFocus type='number' min={0} value={value.length ? value : 0} className='content' onChange={(e) => this.setState({ value: e.target.value })}/>
+						</div>
+						<div className='buttons'>
+							<table className='table'>
+								{values.map((inArr, key) => <tr key={key}>
+									{inArr.map((obj, inKey) => <td
+										onClick={() => this.pressButton(obj.value)}
+										className={obj.className}
+										colSpan={obj.colSpan}
+										rowSpan={obj.rowSpan}
+										key={inKey}> {obj.value}</td>)}
+								</tr>)}
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
+export default Calc;
